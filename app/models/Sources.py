b438f3f60afa57ad909653ea1afc6df5eb91fbad
imekenye <ismekenye@gmail.com>
2019-04-14 10:18:58 -0700

    add sources file

---
+++ app/models/Sources.py
@@ -0,0 +1,10 @@
+class Sources:
+    '''
+    source class to define source Objects
+    '''
+
+    def __init__(self,id,name,description,url):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url 
