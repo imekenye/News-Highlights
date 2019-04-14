1d7e34388c25167024c062c4ede188b3bd7029dd
imekenye <ismekenye@gmail.com>
2019-04-14 15:39:32 -0700

    import get_sources from request file

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 # Views
 @app.route('/')
@@ -9,11 +10,15 @@ def index():
     View root page function that returns the index page and its data
     '''
 
+    news_sources = get_sources('mysources')
+    news_category = get_sources('category')
+    
+
     title = 'Home - Welcome to Your #1 News Source'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, sources = news_sources, category = news_category )
 
 @app.route('/article/<int:article_id>')
-def movie(article_id):
+def article(article_id):
 
     '''
     View article page function that returns the article details page and its data
