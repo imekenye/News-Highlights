e722d251ce5df5546742d01c5cf6494322b8621e
imekenye <ismekenye@gmail.com>
2019-04-14 15:40:03 -0700

    add category object

--- app/models/Sources.py
+++ app/models/Sources.py
@@ -3,8 +3,9 @@ class Sources:
     source class to define source Objects
     '''
 
-    def __init__(self,id,name,description,url):
+    def __init__(self,id,name,description,url,category):
         self.id =id
         self.name = name
         self.description = description
         self.url = url 
+        self.category = category
