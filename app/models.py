b94984340ebb34a99d6dd3e32222a00191294595
imekenye <ismekenye@gmail.com>
2019-04-14 17:57:14 -0700

    add models file to root structure

---
+++ app/models.py
@@ -0,0 +1,25 @@
+class Sources:
+    '''
+    source class to define source Objects
+    '''
+
+    def __init__(self,id,name,description,url,category):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url 
+        self.category = category
+
+class Articles:
+    '''
+    article class to define article Objects
+    '''
+
+    def __init__(self,id,name,title,url,urlToImage,content,publishedAt):
+        self.id =id
+        self.name = name
+        self.title = title
+        self.url = url
+        self.urlToImage = urlToImage
+        self.content = content
+        self.publishedAt = publishedAt        
