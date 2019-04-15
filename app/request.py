f3acc708f0eaa30f6525ac5b8c9677d30a4ef00c
imekenye <ismekenye@gmail.com>
2019-04-14 17:34:57 -0700

    update request file

--- app/request.py
+++ app/request.py
@@ -1,6 +1,6 @@
 from app import app
 import urllib.request,json
-from .models import Sources
+from .models import Sources,Articles
 
 Sources = Sources.Sources
 
@@ -9,6 +9,8 @@ api_key = app.config['NEWS_API_KEY']
 
 # Getting the news base url
 base_url = app.config["NEWS_API_BASE_URL"]
+base2_url = app.config["ARTICLE_API_BASE_URL"]
+
 
 def get_sources(sources):
     '''
@@ -49,3 +51,47 @@ def process_results(sources_list):
         sources_results.append(source_object)
 
     return sources_results
+
+def get_article(article_id):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = base2_url.format(article_id, api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_articles(articles_results_list)
+
+    return articles_results
+
+def process_articles(articles_list):
+    
+    '''
+    This is a function that processes the articles result and transform them to a list of articles
+    Args:
+        articles_list: A list of dictionaries that contain articles
+    Returns :
+        articles_results: A list of article objects
+    ''' 
+    articles_results = []
+    for articles_item in articles_list:
+        id = articles_item.get('id')
+        name = articles_item.get('name')
+        title = articles_item.get('title')
+        url = articles_item.get('url')
+        image = articles_item.get('urlToImage')
+        content = articles_item.get('content')
+        publishedAt = articles_item.get('publishedAt')
+
+
+        if image:
+            article_object= Articles(id, name, title, url, image, content, publishedAt)
+            articles_results.append(article_object)
+
+    return articles_results    
